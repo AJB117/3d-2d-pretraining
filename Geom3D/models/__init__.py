168737feabb7641a5e8eecc3fbe80f4720467dff
Patrick Soga <zqe3cg@virginia.edu>
2024-03-03 23:52:06 -0500

    import egnn

--- Geom3D/models/__init__.py
+++ Geom3D/models/__init__.py
@@ -2,3 +2,4 @@ from .painn import PaiNN
 from .schnet import SchNet
 from .molecule_gnn_model import GNN, GNN_graphpred, GraphPredLinear
 from .autoencoder import AutoEncoder, VariationalAutoEncoder
+from .EGNN import EGNN
