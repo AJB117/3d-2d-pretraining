610125ac8267aadf90c7a5816fd7e855ddc54d82
Patrick Soga <zqe3cg@virginia.edu>
2024-03-07 02:00:39 -0500

    add model path

--- method/pretrain.sh
+++ method/pretrain.sh
@@ -1,3 +1,3 @@
 cd ..;
-PYTHONPATH='.' python runners/pretrain.py --input_data_dir=/home/patrick/3d-2d-pretraining/data --dataset=QM9 --task=gap --model_3d=EGNN --epochs 1 --output_model_dir ./egnn_qm9/assets --lr_scheduler ReduceLROnPlateau --batch_size 128 --verbose --device 1 --mode EGNN --require_3d --num_workers 4
+PYTHONPATH='.' python runners/pretrain.py --input_data_dir=/home/patrick/3d-2d-pretraining/data/molecule_datasets --dataset=QM9 --model_3d=EGNN --epochs 1 --output_model_dir ./egnn_qm9/assets --lr_scheduler ReduceLROnPlateau --batch_size 256 --verbose --device 1 --mode EGNN --require_3d --num_workers 4 --input_model_file_3d /home/patrick/3d-2d-pretraining/egnn_pcqm4mv2/assets/model.pth
 
