6c62485a90ff851236e6dac33536de8a6eeae085
Patrick Soga <zqe3cg@virginia.edu>
2024-03-22 05:37:35 -0400

    tasks + per-block loss + todo for interatomic loss

--- runners/pretrain_deep_interact.py
+++ runners/pretrain_deep_interact.py
@@ -11,16 +11,19 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import subprocess
+import torch.nn.functional as F
 
 from tqdm import tqdm
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import global_mean_pool, global_max_pool, global_add_pool
+from torch_geometric.utils import batched_negative_sampling
 
 from Geom3D.datasets import Molecule3DDataset, MoleculeDatasetQM9
 from Geom3D.models import GNN, SchNet, EGNN, Interactor
 from config import args
 from ogb.utils.features import get_atom_feature_dims
 from util import NTXentLoss, perturb, VirtualNodeMol
+from collections import defaultdict
 
 
 ogb_feat_dim = get_atom_feature_dims()
@@ -30,6 +33,7 @@ ogb_feat_dim[-1] = 0
 
 mae_loss = nn.L1Loss()
 mse_loss = nn.MSELoss()
+bce_loss = nn.BCEWithLogitsLoss()
 
 
 def compute_accuracy(pred, target):
@@ -43,6 +47,8 @@ def save_model(
     graph_pred_mlp,
     model,
     model_name,
+    pretrained_heads_2d=None,
+    pretrained_heads_3d=None,
 ):
     if args.output_model_dir == "":
         return
@@ -50,6 +56,8 @@ def save_model(
     saver_dict = {
         "graph_pred_mlp": graph_pred_mlp.state_dict(),
         "model": model.state_dict(),
+        "pretrained_heads_2d": [head.state_dict() for head in pretrained_heads_2d],
+        "pretrained_heads_3d": [head.state_dict() for head in pretrained_heads_3d],
     }
     if save_best:
         global optimal_loss
@@ -93,17 +101,114 @@ def CL_acc(x1, x2, pos_mask=None):
     return (true_positives / num_positives + true_negatives / num_negatives) / 2
 
 
+def create_pretrain_heads(task, intermediate_dim, device):
+    normalizer = None
+    if args.batch_norm:
+        normalizer = nn.BatchNorm1d(intermediate_dim)
+    elif args.layer_norm:
+        normalizer = nn.LayerNorm(intermediate_dim)
+    else:
+        normalizer = nn.Identity()
+
+    if task == "interatomic_dist":
+        pred_head = nn.Sequential(
+            nn.Linear(intermediate_dim * 2, intermediate_dim),
+            normalizer,
+            nn.ReLU(),
+            nn.Linear(intermediate_dim, 1),
+        )
+    elif task == "edge_existence":
+        pred_head = nn.Sequential(
+            nn.Linear(intermediate_dim * 2, intermediate_dim),
+            normalizer,
+            nn.ReLU(),
+            nn.Linear(intermediate_dim, 1),
+        )
+
+    return pred_head.to(device)
+
+
+def interatomic_distance_loss(batch, embs, pred_head, max_samples=10):
+    """
+    Given a batch of embeddings, predict the interatomic distances
+    with the given head and return the mean squared error loss.
+    """
+    interatomic_distances = batch.atomic_distances
+
+    num_nodes_per_batch = torch.bincount(batch.batch).to(embs.device)
+    num_samples_per_batch = torch.min(
+        torch.tensor(max_samples), num_nodes_per_batch
+    ).to(embs.device)
+
+    pairs = torch.cat(
+        [
+            torch.randint(0, num_nodes_per_batch[i], (num_samples_per_batch[i], 2)).to(
+                embs.device
+            )
+            + torch.sum(num_nodes_per_batch[:i])
+            for i in range(len(num_nodes_per_batch))
+        ],
+        dim=0,
+    ).to(embs.device)
+
+    pair_embs = torch.cat([embs[pairs[:, 0]], embs[pairs[:, 1]]], dim=1).to(embs.device)
+
+    pred_distances = pred_head(pair_embs).squeeze()
+    pdb.set_trace()  # bug: batches could go beyond the 50 atom limit; maybe just compute the distances at runtime
+    true_distances = interatomic_distances[pairs[:, 0], pairs[:, 1]]
+
+    loss = mse_loss(pred_distances, true_distances)
+
+    return loss
+
+
+def edge_existence_loss(batch, embs, pred_head, max_samples=10):
+    """
+    Given a batch of embeddings, predict whether an edge exists between
+    two atoms with the given head and return the binary cross entropy loss.
+    Predict the existence of all edges in the batch and sample negative
+    edges for balance.
+    """
+    pos_links = batch.edge_index
+    pos_link_embs = torch.cat([embs[pos_links[0]], embs[pos_links[1]]], dim=1).to(
+        embs.device
+    )
+
+    neg_links = batched_negative_sampling(
+        batch.edge_index, batch.batch, num_neg_samples=max_samples
+    ).to(embs.device)
+    neg_link_embs = torch.cat([embs[neg_links[0]], embs[neg_links[1]]], dim=1).to(
+        embs.device
+    )
+
+    pred_pos_links = pred_head(pos_link_embs).squeeze()
+    pred_neg_links = pred_head(neg_link_embs).squeeze()
+
+    pos_labels = torch.ones(len(pos_links[0])).to(embs.device)
+    neg_labels = torch.zeros(len(neg_links[0])).to(embs.device)
+
+    pred_edges = torch.cat([pred_pos_links, pred_neg_links], dim=0)
+    true_edges = torch.cat([pos_labels, neg_labels], dim=0)
+
+    loss = bce_loss(pred_edges, true_edges)
+
+    return loss
+
+
 def pretrain(
     args,
     model_name,
-    model,
+    model: Interactor,
     device,
     loader,
     optimizer,
     graph_pred_mlp=None,
     lr_scheduler=None,
     epoch=0,
+    pretrain_heads_2d=None,
+    pretrain_heads_3d=None,
 ):
+    loss_dict = defaultdict(int)
     start_time = time.time()
 
     CL_loss_accum, CL_acc_accum = 0, 0
@@ -119,33 +224,75 @@ def pretrain(
     for step, batch in enumerate(l):
         batch = batch.to(device)
 
+        final_embs, midstream_2d_outs, midstream_3d_outs = model(
+            batch.x,
+            batch.edge_index,
+            batch.edge_attr,
+            batch.positions,
+            batch.batch,
+            require_midstream=True,
+        )
+
+        tasks_2d = args.pretrain_2d_tasks
+        tasks_3d = args.pretrain_3d_tasks
+
+        loss_terms = []
         loss = 0
-        loss_accum += loss
+
+        if args.start_tasks_from_end:
+            pretrain_heads_2d = pretrain_heads_2d[::-1]
+            pretrain_heads_3d = pretrain_heads_3d[::-1]
+            midstream_2d_outs = midstream_2d_outs[::-1]
+            midstream_3d_outs = midstream_3d_outs[::-1]
+
+        for task_2d, pred_head, midstream in zip(
+            tasks_2d, pretrain_heads_2d, midstream_2d_outs
+        ):
+            if task_2d == "interatomic_dist":
+                new_loss = interatomic_distance_loss(batch, midstream, pred_head)
+                loss += new_loss
+
+            loss_terms.append(new_loss)
+            loss_dict[task_2d] += new_loss.item()
+
+        for task_3d, pred_head, midstream in zip(
+            tasks_3d, pretrain_heads_3d, midstream_3d_outs
+        ):
+            if task_3d == "edge_existence":
+                new_loss = edge_existence_loss(batch, midstream, pred_head)
+                loss += new_loss
+
+            loss_terms.append(new_loss)
+            loss_dict[task_3d] += new_loss.item()
+
+        loss_dict["loss_accum"] += loss
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
         if args.lr_scheduler in ["CosineAnnealingWarmRestarts"]:
             lr_scheduler.step(epoch - 1 + step / num_iters)
-            loss_accum /= len(loader)
+            loss_dict["loss_accum"] /= len(loader)
 
     if args.lr_scheduler in ["StepLR", "CosineAnnealingLR"]:
         lr_scheduler.step()
     elif args.lr_scheduler in ["ReduceLROnPlateau"]:
-        lr_scheduler.step(loss_accum)
+        lr_scheduler.step(loss_dict["loss_accum"])
 
     global optimal_loss
 
-    CL_loss_accum /= len(loader)
-    CL_acc_accum /= len(loader)
+    for key in loss_dict:
+        if args.lr_scheduler in ["CosineAnnealingWarmRestarts"] and key == "loss_accum":
+            continue
 
-    loss_accum /= len(loader)
+        loss_dict[key] /= len(loader)
 
-    print("Total Loss: {:.5f}".format(loss_accum))
+    print("Total Loss: {:.5f}".format(loss_dict["loss_accum"]))
 
     print("Time: {:.5f}\n".format(time.time() - start_time))
 
-    temp_loss = loss_accum
+    temp_loss = loss_dict["loss_accum"]
 
     if temp_loss < optimal_loss:
         optimal_loss = temp_loss
@@ -154,15 +301,14 @@ def pretrain(
             graph_pred_mlp=graph_pred_mlp,
             model=model,
             model_name=model_name,
+            pretrained_heads_2d=pretrain_heads_2d,
+            pretrained_heads_3d=pretrain_heads_3d,
         )
 
-    loss_dict = {
-        "CL_loss_accum": CL_loss_accum.item(),
-        "CL_acc_accum": CL_acc_accum.item(),
-        "loss_accum": loss_accum.item(),
-    }
+    loss_dict["loss_accum"] = loss_accum.item()
 
-    wandb.log(loss_dict)
+    if args.wandb:
+        wandb.log(loss_dict)
 
     return loss_dict
 
@@ -181,7 +327,9 @@ def main():
 
     num_node_classes = 119 + 1  # add 1 for virtual node
 
-    transform = VirtualNodeMol()
+    transform = (
+        VirtualNodeMol() if args.interaction_rep_3d in ("com", "const_radius") else None
+    )
     data_root = "{}/{}".format(args.input_data_dir, args.dataset)
     try:
         dataset = Molecule3DDataset(
@@ -195,7 +343,7 @@ def main():
     except FileNotFoundError:
         if args.dataset == "QM9":
             data_root = "data/molecule_datasets/{}".format(args.dataset)
-            use_pure_atomic_num = True
+            use_pure_atomic_num = False
             if args.model_3d == "EGNN":
                 use_pure_atomic_num = False
             MoleculeDatasetQM9(
@@ -203,7 +351,7 @@ def main():
                 dataset=args.dataset,
                 task=args.task,
                 use_pure_atomic_num=use_pure_atomic_num,
-            ).process()
+            )
         dataset = Molecule3DDataset(
             data_root,
             args.dataset,
@@ -233,7 +381,7 @@ def main():
         dropout=args.dropout_ratio,
         batch_norm=args.batch_norm,
         layer_norm=args.layer_norm,
-    )
+    ).to(device)
 
     if args.JK == "concat":
         intermediate_dim = (args.num_layer + 1) * args.emb_dim
@@ -254,14 +402,23 @@ def main():
             normalizer,
             nn.ReLU(),
             nn.Linear(intermediate_dim, 1),
-        )
+        ).to(device)
     else:
         graph_pred_mlp = nn.Sequential(
             nn.Linear(intermediate_dim, intermediate_dim),
             normalizer,
             nn.ReLU(),
             nn.Linear(intermediate_dim, 1),
-        )
+        ).to(device)
+
+    pretrain_heads_2d = [
+        create_pretrain_heads(task, intermediate_dim, device)
+        for task in args.pretrain_2d_tasks
+    ]
+    pretrain_heads_3d = [
+        create_pretrain_heads(task, intermediate_dim, device)
+        for task in args.pretrain_3d_tasks
+    ]
 
     for layer in graph_pred_mlp:
         if isinstance(layer, nn.Linear):
@@ -276,6 +433,10 @@ def main():
 
     # non-GNN components
     model_param_group.append({"params": graph_pred_mlp.parameters(), "lr": args.lr})
+    for head in pretrain_heads_2d:
+        model_param_group.append({"params": head.parameters(), "lr": args.lr})
+    for head in pretrain_heads_3d:
+        model_param_group.append({"params": head.parameters(), "lr": args.lr})
 
     # GNNs
     model_param_group.append(
@@ -322,7 +483,9 @@ def main():
         model_name = f"{now_in_ms}"
 
     args_dict = vars(args)
-    wandb.init(project="molecular-pretraining", config=args_dict)
+
+    if args.wandb:
+        wandb.init(project="molecular-pretraining", config=args_dict)
 
     for epoch in range(1, args.epochs + 1):
         print("epoch: {}".format(epoch))
@@ -336,6 +499,8 @@ def main():
             graph_pred_mlp=graph_pred_mlp,
             lr_scheduler=lr_scheduler,
             epoch=epoch,
+            pretrain_heads_2d=pretrain_heads_2d,
+            pretrain_heads_3d=pretrain_heads_3d,
         )
 
     # to aggregate later with a separate script
@@ -385,9 +550,12 @@ def main():
         graph_pred_mlp=graph_pred_mlp,
         model=model,
         model_name=model_name,
+        pretrained_heads_2d=pretrain_heads_2d,
+        pretrained_heads_3d=pretrain_heads_3d,
     )
 
-    wandb.finish()
+    if args.wandb:
+        wandb.finish()
 
     return config_id
 
