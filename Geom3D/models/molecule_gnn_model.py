d56b60fbd573ff1c6bc70ef21f2c64e43e5754d2
Patrick <psoga117@gmail.com>
2024-04-14 01:55:59 -0400

    ginblock: reduced dim mlp + no bnorm in gin block

--- Geom3D/models/molecule_gnn_model.py
+++ Geom3D/models/molecule_gnn_model.py
@@ -53,6 +53,35 @@ class TransformerConv(MessagePassing):
         return aggr_out
 
 
+class GINBlock(MessagePassing):
+    def __init__(self, emb_dim):
+        super(GINBlock, self).__init__(aggr="add")
+
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(emb_dim, emb_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_dim, emb_dim),
+        )
+        self.eps = torch.nn.Parameter(torch.Tensor([0]))
+
+        self.bond_encoder = BondEncoder(emb_dim=emb_dim)
+
+    def forward(self, x, edge_index, edge_attr):
+        edge_embedding = self.bond_encoder(edge_attr)
+        out = self.mlp(
+            (1 + self.eps) * x
+            + self.propagate(edge_index, x=x, edge_attr=edge_embedding)
+        )
+
+        return out
+
+    def message(self, x_j, edge_attr):
+        return F.relu(x_j + edge_attr)
+
+    def update(self, aggr_out):
+        return aggr_out
+
+
 class GINConv(MessagePassing):
     def __init__(self, emb_dim):
         super(GINConv, self).__init__(aggr="add")
