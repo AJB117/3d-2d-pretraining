a2da8163823bb6b7bc5223aa28b99f4617958602
Patrick <psoga117@gmail.com>
2024-04-20 23:13:12 -0400

    use whole dataset for eval not just batch

--- runners/finetune_PCQM4Mv2_deep_interact.py
+++ runners/finetune_PCQM4Mv2_deep_interact.py
@@ -163,7 +163,7 @@ def train(epoch, device, loader, optimizer):
     loss_acc = 0
     num_iters = len(loader)
 
-    y_scores = []
+    y_true, y_scores = [], []
 
     if args.verbose:
         L = tqdm(loader)
@@ -209,6 +209,7 @@ def train(epoch, device, loader, optimizer):
         if args.lr_scheduler in ["CosineAnnealingWarmRestarts"]:
             lr_scheduler.step(epoch - 1 + step / num_iters)
 
+        y_true.append(y)
         y_scores.append(pred_denorm)
 
     loss_acc /= len(loader)
@@ -217,8 +218,10 @@ def train(epoch, device, loader, optimizer):
     elif args.lr_scheduler in ["ReduceLROnPlateau"]:
         lr_scheduler.step(loss_acc)
 
+    y_true = torch.cat(y_true, dim=0).cpu().numpy()
     y_scores = torch.cat(y_scores, dim=0).cpu().numpy()
-    mae = mean_absolute_error(y_scores, batch.y.cpu().numpy())
+
+    mae = mean_absolute_error(y_scores, y_true)
 
     return loss_acc, mae
 
