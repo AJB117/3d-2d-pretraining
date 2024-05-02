2fa2d4a83b0509d21e9f23cfa3f1ff7435bd5449
Patrick Soga <zqe3cg@virginia.edu>
2024-05-02 04:22:38 -0400

    opt for pretraining

--- runners/generate_PCQM4Mv2.py
+++ runners/generate_PCQM4Mv2.py
@@ -2,5 +2,7 @@ from Geom3D.datasets import PCQM4Mv2
 
 
 if __name__ == "__main__":
+    # root_dir = "/home/patrick/data/PCQM4Mv2-pretraining"
+    # test_dataset = PCQM4Mv2(root=root_dir, transform=None, pretraining=True)
     root_dir = "/home/patrick/data/PCQM4Mv2"
-    test_dataset = PCQM4Mv2(root=root_dir, transform=None)
+    dataset = PCQM4Mv2(root=root_dir, transform=None, pretraining=False)
