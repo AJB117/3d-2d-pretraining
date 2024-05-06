d92b536d41d056bf81d29414caf04399e37ccfad
Patrick <psoga117@gmail.com>
2024-05-06 13:34:07 -0400

    add centrality ranking loss to the ensemble

--- runners/pretrain_deep_interact.py
+++ runners/pretrain_deep_interact.py
@@ -372,6 +372,10 @@ def pretrain(
                             pretrain_heads_3d[i],
                             dihedral_angle_indices,
                         )
+                    elif task_3d == "centrality_ranking":
+                        loss_3d = centrality_ranking_loss(
+                            batch, midstream, pred_head, sample_edges
+                        )
 
                     loss = loss + loss_2d + loss_3d
                     loss_terms.append(loss)
